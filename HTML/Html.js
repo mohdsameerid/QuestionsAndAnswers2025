4a892392219cfa758ce8e8887a2b92253307a2c3
Mohd Samir <mohdsameeredu@gmail.com>
2025-03-24 15:42:48 +0530

    Added question

---
+++ HTML/Html.js
@@ -0,0 +1,11 @@
+
+// 1. What does H, T, M , L stand for in HTML and explain each word in detail. ?
+// Ans :
+// HTML stands for **HyperText Markup Language**:
+
+// - **Hyper** → Links between web pages (hyperlinks).
+// - **Text** → Content displayed on web pages.
+// - **Markup** → Tags (`<p>`, `<h1>`, etc.) that structure content.
+// - **Language** → Standard set of rules for web page creation.
+
+// It's the foundation of web development, used to structure web pages. 
